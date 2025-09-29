dafbd7b5e52332e510c71afc9f3264a0037f10d2
Anton211 <mranton21@bk.ru>
2025-09-29 08:49:16 +0300

    fixes

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -60,7 +60,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000,http://90.156.218.233:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://90.156.218.233")
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
