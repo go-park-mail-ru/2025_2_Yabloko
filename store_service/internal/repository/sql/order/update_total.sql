b639eae0acf18c510546b083449806fee01beac1
pirog555 <Do4ka.ad@yandex.ru>
2025-11-10 15:34:42 +0300

    fix

--- store_service/internal/repository/sql/order/update_total.sql
+++ store_service/internal/repository/sql/order/update_total.sql
@@ -1,9 +1,13 @@
-UPDATE orders
-SET total_price = (
-    SELECT SUM(si.price * ci.quantity)
-    FROM cart_item ci
-    JOIN cart c on c.id = ci.cart_id
-    JOIN store_item si on si.id = ci.store_item_id
-    WHERE c.user_id = $1
-)
-WHERE id = $2;
+UPDATE
+    orders
+SET
+    total_price = (
+        SELECT
+            COALESCE(SUM(oi.price * oi.quantity), 0)
+        FROM
+            order_item oi
+        WHERE
+            oi.order_id = $1
+    )
+WHERE
+    id = $1;
