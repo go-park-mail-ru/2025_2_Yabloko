81d53be3b340019c65025b0ca4a1d81dca8da100
pirog555 <Do4ka.ad@yandex.ru>
2025-11-15 16:53:38 +0300

    cmd app

---
+++ support_service/cmd/app.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"apple_backend/support_service/internal/config"
+	shttp "apple_backend/support_service/internal/delivery/http"
+	"apple_backend/support_service/internal/delivery/middlewares"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Run() {
+	conf := config.MustConfig()
+	apiV0Prefix := "/api/v0/"
+
+	dbPool, err := pgxpool.New(context.Background(), conf.DBPath())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dbPool.Close()
+
+	openMux := http.NewServeMux()
+
+	shttp.NewSupportRouter(openMux, dbPool, apiV0Prefix)
+
+	mux := http.NewServeMux()
+	mux.Handle(apiV0Prefix, openMux)
+
+	handler := middlewares.CORS(mux)
+	addr := fmt.Sprintf("0.0.0.0:%s", conf.AppPort)
+	log.Printf("Support service running on http://localhost:%s", conf.AppPort)
+	log.Fatal(http.ListenAndServe(addr, handler))
+}
