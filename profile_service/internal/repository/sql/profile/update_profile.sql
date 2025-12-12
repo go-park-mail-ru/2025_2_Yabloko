3409b085ff0869e67374ba5b2190e1109307d37f
StrayDog31 <s.yakovlev06@yahoo.com>
2025-12-13 02:14:28 +0300

    fix

--- profile_service/internal/repository/sql/profile/update_profile.sql
+++ profile_service/internal/repository/sql/profile/update_profile.sql
@@ -1,23 +1,20 @@
 UPDATE account
 SET
-    name   = $1,
-    phone  = $2,
+    name    = $1,
+    phone   = $2,
     city_id = $3,
     address = $4,
     addresses_history = CASE
-        WHEN $5 IS NOT NULL THEN
+        WHEN $5::jsonb IS NOT NULL THEN
             $5::jsonb
-
         ELSE
             CASE
                 WHEN $4 IS NULL OR btrim($4) = '' THEN
                     COALESCE(addresses_history, '[]'::jsonb)
-
                 ELSE
                     CASE
                         WHEN jsonb_array_length(COALESCE(addresses_history, '[]'::jsonb)) = 0 THEN
                             to_jsonb(ARRAY[btrim($4)]::text[])
-
                         ELSE
                             CASE
                                 WHEN (COALESCE(addresses_history, '[]'::jsonb) ->> -1)
