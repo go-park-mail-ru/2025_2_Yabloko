1061a6a3808c50697c4c99f4158d20a31804b009
StrayDog31 <s.yakovlev06@yahoo.com>
2025-12-13 02:25:05 +0300

    fix

--- profile_service/internal/repository/sql/profile/update_profile.sql
+++ profile_service/internal/repository/sql/profile/update_profile.sql
@@ -3,29 +3,32 @@ SET
     name    = $1,
     phone   = $2,
     city_id = $3,
-    address = $4,
+
+    address = $4::text,
+
     addresses_history = CASE
         WHEN $5::jsonb IS NOT NULL THEN
             $5::jsonb
         ELSE
             CASE
-                WHEN $4 IS NULL OR btrim($4) = '' THEN
+                WHEN $4::text IS NULL OR btrim($4::text) = '' THEN
                     COALESCE(addresses_history, '[]'::jsonb)
                 ELSE
                     CASE
                         WHEN jsonb_array_length(COALESCE(addresses_history, '[]'::jsonb)) = 0 THEN
-                            to_jsonb(ARRAY[btrim($4)]::text[])
+                            to_jsonb(ARRAY[btrim($4::text)]::text[])
                         ELSE
                             CASE
                                 WHEN (COALESCE(addresses_history, '[]'::jsonb) ->> -1)
-                                      IS DISTINCT FROM btrim($4) THEN
+                                      IS DISTINCT FROM btrim($4::text) THEN
                                     COALESCE(addresses_history, '[]'::jsonb)
-                                    || to_jsonb(ARRAY[btrim($4)]::text[])
+                                    || to_jsonb(ARRAY[btrim($4::text)]::text[])
                                 ELSE
                                     COALESCE(addresses_history, '[]'::jsonb)
                             END
                     END
             END
     END,
-    avatar_url = $6
-WHERE id = $7;
+
+    avatar_url = $6::text
+WHERE id = $7::uuid
